9a5bbb2c5cc0ddede53a3e475a10995ce4003162
Jordan Williams <jjwilli13@gmail.com>
2022-07-27 21:11:13 -0500

    finalzed variable excercise

---
+++ WorkingWithVariables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WorkingWithVariables  
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Copper";
+            int dogAge = 4;
+            char nickName = 'C';
+            bool hatesBaths = true;
+            double dogBones = 10;
+            decimal weight = 12.2m;
+
+            Console.WriteLine($"My dogs name is {dogName} but we call him {nickName}, and " +
+                $"he is {dogAge} years old. He weighs {weight} pounds." +
+                $"It is also {hatesBaths} that he hates baths.");
+
+        
+
+            
+        }
+    }
+}
+
